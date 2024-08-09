9e42164750310d09bd587719299f1731e171e4d6
Khang Nguyen Phuc <khangnguyen261102@gmail.com>
2024-08-09 22:56:49 +0700

    feat: add customer-server

---
+++ services/customer-server/src/test/java/com/khangng/customer_server/CustomerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khangng.customer_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
