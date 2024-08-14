dab977e6da376b148399f12f06def219a3029ace
Khang Nguyen Phuc <khangnguyen261102@gmail.com>
2024-08-14 15:47:03 +0700

    feat: order service - init

---
+++ services/order-service/src/main/java/com/khangng/order_service/entity/Order.java
@@ -0,0 +1,22 @@
+package com.khangng.order_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+}
