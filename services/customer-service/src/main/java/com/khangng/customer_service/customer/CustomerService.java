0d59da5466241170ec5baabd4c52c6ce09635f3c
Khang Nguyen Phuc <khangnguyen261102@gmail.com>
2024-08-20 11:30:06 +0700

    refactor: re-define port for services

--- services/customer-service/src/main/java/com/khangng/customer_service/customer/CustomerService.java
+++ services/customer-service/src/main/java/com/khangng/customer_service/customer/CustomerService.java
@@ -56,6 +56,8 @@ public class CustomerService {
     }
     
     public void deleteCustomer(String customerId) {
+        customerRepository.findById(customerId)
+                .orElseThrow(() -> new CustomerNotFoundException(String.format("No customer found with the provided ID: %s", customerId)));
         customerRepository.deleteById(customerId);
     }
 }
