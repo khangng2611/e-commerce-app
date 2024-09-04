de5083ad7b9389d36d3dd77e209c097a92bfe7b1
Khang Nguyen Phuc <khangnguyen261102@gmail.com>
2024-09-04 22:38:09 +0700

    feat: gateway service - setup authorization for /customers api

--- services/gateway-service/src/main/java/com/khangng/gateway_service/security/SecurityConfig.java
+++ services/gateway-service/src/main/java/com/khangng/gateway_service/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.khangng.gateway_service.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -19,9 +20,14 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/eureka/**").permitAll()
-//                        .pathMatchers("/api/v1/customers/**").hasRole("ADMIN_WRITE")
+                        
+                        // customer
+                        .pathMatchers("/api/v1/customers/me").authenticated()
+                        .pathMatchers(HttpMethod.GET, "/api/v1/customers/**").hasRole("ADMIN_WRITE")
+                        .pathMatchers(HttpMethod.PUT, "/api/v1/customers/**").hasRole("ADMIN_WRITE")
+                        .pathMatchers(HttpMethod.DELETE, "/api/v1/customers/**").hasRole("ADMIN_WRITE")
+                        
                         .anyExchange().authenticated()
-//                        .anyExchange().permitAll()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt().jwtAuthenticationConverter(new KeycloakJwtConverter(kcClientId)));
         return http.build();
